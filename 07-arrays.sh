03c04040cd87214fe55cc96b1ed265468f9941f3
sayed <sumez180@gmail.com>
2026-03-18 17:02:45 +0530

    added 2nd script

---
+++ 07-arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("apple" , "banana" , "mango")
+
+echo " first name: ${FRUITS[0]}"
+echo " second name: ${FRUITS[1]}"
+echo " third name: ${FRUITS[2]}"
+echo " all fruits: ${FRUITS[@]}"
